a33f83d87680f3b9b3a7ba582318817208a91339
Miguel Romero Suarez <54338977+miguelromerosuarez@users.noreply.github.com>
2025-03-03 13:51:01 -0500

    Add files via upload

---
+++ fitness-tracker/src/firebaseConfig.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1pzXpq_BvES9j2Exr5KpmwEjRcF9wnOs",
+  authDomain: "fitnesstracker-4488c.firebaseapp.com",
+  projectId: "fitnesstracker-4488c",
+  storageBucket: "fitnesstracker-4488c.firebasestorage.app",
+  messagingSenderId: "895168275825",
+  appId: "1:895168275825:web:6c2d024fd6890a60f942d0",
+  measurementId: "G-FQF9K0S570"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
